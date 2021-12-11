d2f149c3b04bc22ba00fd03450ad70021f5e3267
leilahsmith <leilah.sdev@gmail.com>
2021-12-11 00:18:02 -0300

    Added Unit Testing Class for Komodo Badge

---
+++ KomodoBadgeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KomodoBadgeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KomodoBadgeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d3540a3-b460-4e17-b955-f628715f9988")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KomodoBadgeTests/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using KomodoBadges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace KomodoBadgeTests
+{
+    [TestClass]
+    public class Badge_RepoTests
+    {
+        [TestMethod]
+        public void AddToRepo_ShouldGetCorrectBool()//Create
+        {
+            //Arrange
+            BadgeListPoco badge = new BadgeListPoco(01);
+            BadgeRepo repo = new BadgeRepo();
+
+            //Act
+            bool addResult = repo.AddBadgeToList(badge);
+
+            //Assert
+            Assert.IsTrue(addResult);
+        }
+
+        [TestMethod]
+        public void GetBadges_ShouldReturnCorrectCollection() //Read
+        {
+
+           
+            //Arrange
+            BadgeListPoco badge = new BadgeListPoco(01);
+            BadgeRepo repo = new BadgeRepo();
+            repo.AddBadgeToList(badge);
+
+            //Act
+            Dictionary<int, List<string>> badges = repo.ViewExistingBadges();
+            bool hasBadges = badges.ContainsKey(badge.BadgeID);
+
+            //Assert
+            Assert.IsTrue(hasBadges);
+
+        }
+
+        [TestMethod]
+        public void GetBadgeByID_ShouldReturnCorrectBadge() //Read
+        {
+            //Arrange
+            //Arrange
+            BadgeListPoco badge = new BadgeListPoco(001, new List<string> { "A1", "A2" });
+            BadgeRepo repo = new BadgeRepo();
+            
+            
+            repo.AddBadgeToList(badge);
+            int badgeID = 001;
+            //Act
+            BadgeListPoco searchResult = repo.GetABadgeByID(badgeID);
+
+            //Assert
+            Assert.AreEqual(searchResult.BadgeID, badgeID);
+        }
+
+        [TestMethod]
+        public void UpdateExistingBadge_ShouldReturnTrue() //Update
+        {
+            //Arrange
+
+            BadgeListPoco oldBadge = new BadgeListPoco(001, new List<string> { "A1", "A2" });
+            BadgeListPoco newBadge = new BadgeListPoco(001, new List<string> { "A2", "A3", "B5" });
+            BadgeRepo repo = new BadgeRepo();
+            repo.AddBadgeToList(oldBadge);
+
+            //Act
+            bool updateResult = repo.UpdateExistingBadge(oldBadge.BadgeID, newBadge);
+
+            //Assert
+            Assert.IsTrue(updateResult);
+        }
+
+        [TestMethod]
+        public void DeleteBadge_ShouldReturnTrue() //Delete
+        {
+            //Arrange
+            BadgeListPoco badge = new BadgeListPoco(001, new List<string> { "A1", "A2" });
+            BadgeRepo repo = new BadgeRepo();
+            repo.AddBadgeToList(badge);
+            int badgeID = 001;
+            //Act
+            BadgeListPoco oldBadge = repo.GetABadgeByID(badgeID);
+            bool removeResult = repo.DeleteExistingAccessOnBadges(oldBadge);
+
+            //Assert
+            Assert.IsTrue(removeResult);
+        }
+    }
+}
